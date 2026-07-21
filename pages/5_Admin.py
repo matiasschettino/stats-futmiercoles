89704be52f1add5fbd917399a2e25be61c248bfa
matiasschettino <114623848+matiasschettino@users.noreply.github.com>
2026-07-21 15:50:31 -0300

    Update 5_Admin.py

--- pages/5_Admin.py
+++ pages/5_Admin.py
@@ -70,7 +70,7 @@ if not jugadores_df.empty:
 # TABS
 # ==================================================
 
-tab1, tab2, tab3 = st.tabs(
+tab1, tab2, tab3, tab4 = st.tabs(
     [
         "⚽ Equipos",
         "👤 Jugadores",
