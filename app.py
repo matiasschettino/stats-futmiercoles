1ed90ae05070d32cbb70e0b89125682802fbfcff
matiasschettino <114623848+matiasschettino@users.noreply.github.com>
2026-07-21 10:43:26 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Futbol Stats",
+    page_icon="⚽",
+    layout="wide"
+)
+
+jugadores = pd.read_csv(
+    "data/jugadores_master.csv"
+)
+
+equipos = pd.read_csv(
+    "data/equipos_master.csv"
+)
+
+st.title("⚽ Fútbol Histórico")
+
+col1, col2 = st.columns(2)
+
+col1.metric(
+    "Jugadores",
+    len(jugadores)
+)
+
+col2.metric(
+    "Equipos",
+    len(equipos)
+)
+
+st.subheader(
+    "🏆 Top 20 jugadores con más partidos"
+)
+
+st.dataframe(
+    jugadores
+    .sort_values("PJ", ascending=False)
+    .head(20)
+)
